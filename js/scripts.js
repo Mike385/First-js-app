3c3c405840927a7d020cc8f3190e3a6036be71a4
Mike385 <mike.kurzawski@gmail.com>
2020-11-23 14:38:29 +0000

    Update scripts.js
    If applied, these changes will show a forEach() Loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 let pokemonList=[
-    {name:"Charizard", height: 1.7, type: ["fire", "flying"]},
-    {name:"Metagross", height: 1.6, type: ["psychic", "steel"]},
-    {name:"Zekrom", height: 2.9, type: ["dragon", "electric"]}
+    {name:"Charizard", 
+    height: 1.7, 
+    type: ["fire", "flying"]
+    },
+    {name:"Metagross", 
+    height: 1.6, 
+    type: ["psychic", "steel"]
+    },
+    {name:"Zekrom", 
+    height: 2.9, 
+    type: ["dragon", "electric"]
+    }
 ];
-
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height >2.8) {
-        document.write(pokemonList[i].name + " Height: " + pokemonList[i].height + " - Wow that's big!");
-    } else {
-        document.write(pokemonList[i].name + " Height: " + pokemonList[i].height) 
-    }  
-    document.write("<br>")
+function myLoopFunction(pokemonList) {
+  document.write(pokemonList.name + ' height: ' + pokemonList.height);
 }
+pokemonList.forEach(myLoopFunction);
+document.write ("<br>")
