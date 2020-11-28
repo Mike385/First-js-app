088628196572a9431b9a5c2a6f0f07f0cb8f68a4
Mike385 <mike.kurzawski@gmail.com>
2020-11-28 14:54:53 +0000

    Update scripts.js
    If applied, this commit will add the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,55 @@ function myLoopFunction(pokemonList) {
   document.write(pokemonList.name + ' height: ' + pokemonList.height);
 }
 pokemonList.forEach(myLoopFunction);
-document.write ("<br>")
+document.write ("<br>");
+
+(function (){
+    let pokemonList=[
+        {name:"Charizard", 
+        height: 1.7, 
+        type: ["fire", "flying"]
+        },
+        {name:"Metagross", 
+        height: 1.6, 
+        type: ["psychic", "steel"]
+        },
+        {name:"Zekrom", 
+        height: 2.9, 
+        type: ["dragon", "electric"]
+        }
+    ];
+    function myLoopFunction(pokemonList) {
+    document.write(pokemonList.name + ' height: ' + pokemonList.height);
+    }
+    pokemonList.forEach(myLoopFunction);
+    })();
+    
+    var pokemonRepository = (function (){
+      var pokemonList = [
+        {name:"Charizard", 
+        height: 1.7, 
+        type: ["fire", "flying"]
+        },
+        {name:"Metagross", 
+        height: 1.6, 
+        type: ["psychic", "steel"]
+        },
+        {name:"Zekrom", 
+        height: 2.9, 
+        type: ["dragon", "electric"]
+        }
+      ];
+    
+    function getAll() {
+      return pokemonList;
+    }
+    function add(pokemon) {
+      pokemonList.push(pokemon); 
+    }
+    return {
+      getAll: getAll,
+      add: add
+    };
+    })();
+    
+    document.write( pokemonRepository.getAll());
