bad74ed160491f3538fd730254abcaa31f3ef5f0
Mike385 <mike.kurzawski@gmail.com>
2020-12-04 20:22:36 +0000

    If applied, this update will have the 'add' and 'getAll' functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,3 @@
-let pokemonList=[
-    {name:"Charizard", 
-    height: 1.7, 
-    type: ["fire", "flying"]
-    },
-    {name:"Metagross", 
-    height: 1.6, 
-    type: ["psychic", "steel"]
-    },
-    {name:"Zekrom", 
-    height: 2.9, 
-    type: ["dragon", "electric"]
-    }
-];
-function myLoopFunction(pokemonList) {
-  document.write(pokemonList.name + ' height: ' + pokemonList.height);
-}
-pokemonList.forEach(myLoopFunction);
-document.write ("<br>");
-
-(function (){
-    let pokemonList=[
-        {name:"Charizard", 
-        height: 1.7, 
-        type: ["fire", "flying"]
-        },
-        {name:"Metagross", 
-        height: 1.6, 
-        type: ["psychic", "steel"]
-        },
-        {name:"Zekrom", 
-        height: 2.9, 
-        type: ["dragon", "electric"]
-        }
-    ];
-    function myLoopFunction(pokemonList) {
-    document.write(pokemonList.name + ' height: ' + pokemonList.height);
-    }
-    pokemonList.forEach(myLoopFunction);
-    })();
-    
     var pokemonRepository = (function (){
       var pokemonList = [
         {name:"Charizard", 
@@ -59,12 +18,20 @@ document.write ("<br>");
       return pokemonList;
     }
     function add(pokemon) {
-      pokemonList.push(pokemon); 
+        if(typeof pokemon==='object'){
+        pokemonList.push(pokemon); 
+        }
     }
     return {
       getAll: getAll,
       add: add
     };
     })();
-    
-    document.write( pokemonRepository.getAll());
+    pokemonRepository.add({
+        name: "Pikachu",
+        height: 0.4,
+        type: ["water", "magic"]
+    })
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        document.write('Name: ' + pokemon.name + ' Height: ' + pokemon.height + '<br>');
+      });
